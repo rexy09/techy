3b8a3a507234a043d7433b5f179958cb5eabd226
rexy09 <fredmuju@gmail.com>
2022-05-01 18:08:25 +0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,14 @@ export default function Home() {
         <link rel="icon" href="/images/techy_1.png" />
 
         {/* Boostrap */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
         {/* Fonts */}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-white" id='#'>
-        <div class="container-fluid px-lg-5 py-3">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white" id='#'>
+        <div className="container-fluid px-lg-5 py-3">
+          <a className="navbar-brand" href="#">
             <Image
               src="/images/techy_1.png"
               alt="Image"
@@ -26,25 +26,25 @@ export default function Home() {
               height={43}
             />
           </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Home</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#technologies">Technologies</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#technologies">Technologies</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#services">Services</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#services">Services</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#cloud">Cloud</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#cloud">Cloud</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#contact">Contact Us</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact Us</a>
               </li>
 
             </ul>
@@ -58,24 +58,24 @@ export default function Home() {
             <div className='row py-5 px-lg-5'>
               <div className='col-lg-6'>
 
-                <div class="d-flex justify-content-center">
+                <div className="d-flex justify-content-center">
                   <div>
                     <h1 className={styles.text_1}>Let's make you,</h1>
                     <h1 className={styles.text_1}>an eye catching</h1>
                     <h1 className={styles.text_2}>designs.</h1>
                     <p className={styles.text_3}>Stand out from the rest with your very own <br />
                       elegant and unique design.</p>
-                    <a class="" href="#">
+                    <a className="" href="#">
                       <div className={styles.button_1}>
-                        <div class="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
+                        <div className="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
                           <div>
-                            Book now <i class="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
+                            Book now <i className="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
                           </div>
                         </div>
                       </div>
                     </a>
 
-                    <div class="d-flex flex-row justify-content-between mt-5">
+                    <div className="d-flex flex-row justify-content-between mt-5">
                       <div>
                         <span className={styles.text_4}>21</span>
                         <br />
@@ -112,7 +112,7 @@ export default function Home() {
 
 
         <div className='my-5' style={{ backgroundColor: "#5c415d1a" }} >
-          <div class="d-flex flex-row justify-content-around mt-5 align-items-center">
+          <div className="d-flex flex-row justify-content-around mt-5 align-items-center">
             <div>
               <Image
                 src="/images/Django.png"
@@ -169,7 +169,7 @@ export default function Home() {
           <div className='container-fluid'>
             <div className='row py-5 mt-5 mx-0'>
               <div className='col-lg-6'>
-                <div class="d-flex justify-content-center">
+                <div className="d-flex justify-content-center">
                   <Image
                     src="/images/Group_1.png"
                     alt="Image"
@@ -191,11 +191,11 @@ export default function Home() {
                     Think of a news blog that's filled with content hourly on
                     <br />
                     the day of going live. </p>
-                  <a class="" href="#">
+                  <a className="" href="#">
                     <div className={styles.button_2}>
-                      <div class="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
+                      <div className="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
                         <div>
-                          Book now <i class="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
+                          Book now <i className="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
                         </div>
                       </div>
                     </div>
@@ -209,7 +209,7 @@ export default function Home() {
       </div>
       <section id='services'>
         <div className='container-fluid mb-5'>
-          <div class="d-flex justify-content-center">
+          <div className="d-flex justify-content-center">
             <div>
               <h1 ><span className={styles.text_9}>Services</span>  <span className={styles.text_10}>we deliver</span></h1>
 
@@ -226,9 +226,9 @@ export default function Home() {
                     width={125}
                     height={125}
                   />
-                  <div class="card-body">
+                  <div className="card-body">
                     <h5 className={styles.text_12}>Product Engineering</h5>
-                    <ul class="list-unstyled">
+                    <ul className="list-unstyled">
                       <li className={styles.text_14}>Web Development</li>
                       <li className={styles.text_14}>Mobile Development</li>
                       <li className={styles.text_14}>Custom Software Development</li>
@@ -248,9 +248,9 @@ export default function Home() {
                     width={125}
                     height={125}
                   />
-                  <div class="card-body">
+                  <div className="card-body">
                     <h5 className={styles.text_13}>Product Design</h5>
-                    <ul class="list-unstyled">
+                    <ul className="list-unstyled">
                       <li className={styles.text_15}>Design Sprint </li>
                       <li className={styles.text_15}>Visual Brand Identity</li>
                       <li className={styles.text_15}>Prototyping</li>
@@ -270,9 +270,9 @@ export default function Home() {
                     width={125}
                     height={125}
                   />
-                  <div class="card-body">
+                  <div className="card-body">
                     <h5 className={styles.text_12}>Technology Consultancy</h5>
-                    <ul class="list-unstyled">
+                    <ul className="list-unstyled">
                       <li className={styles.text_14}>Digital Transformation</li>
                       <li className={styles.text_14}>CTO as a service </li>
                       <li className={styles.text_14}>Discovery Phase </li>
@@ -292,7 +292,7 @@ export default function Home() {
         <div className={`container-fluid ${styles.bg_img_3}`}>
           <div className='row py-5 mt-5 mx-0'>
             <div className='col-lg-6'>
-              <div class="d-flex justify-content-center">
+              <div className="d-flex justify-content-center">
                 <div >
                   <h1 className={styles.text_6}>Our</h1>
                   <h1 className={`${styles.text_9} mb-5`}>Cloud Providers</h1>
@@ -305,11 +305,11 @@ export default function Home() {
                     Think of a news blog that's filled with content hourly on
                     <br />
                     the day of going live. </p>
-                  <a class="" href="#">
+                  <a className="" href="#">
                     <div className={styles.button_2}>
-                      <div class="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
+                      <div className="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
                         <div>
-                          Book now <i class="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
+                          Book now <i className="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
                         </div>
                       </div>
                     </div>
@@ -318,7 +318,7 @@ export default function Home() {
               </div>
             </div>
             <div className='col-lg-6'>
-              <div class="d-flex justify-content-center">
+              <div className="d-flex justify-content-center">
                 <Image
                   src="/images/Group_1.png"
                   alt="Image"
@@ -334,10 +334,10 @@ export default function Home() {
 
      <section id='contact'>
         <div className='container mb-5'>
-          <div class="d-flex justify-content-center">
+          <div className="d-flex justify-content-center">
             <div className={[styles.container_2, styles.bg_img_4].join(" ")}>
               <div className='px-lg-5 py-4'>
-                <div class="d-flex justify-content-center">
+                <div className="d-flex justify-content-center">
                   <div>
                     <h1 ><span className={styles.text_16}>Book now</span></h1>
 
@@ -345,14 +345,14 @@ export default function Home() {
                 </div>
                 <p className={styles.text_17}>Enter your email address to book now
                   <br />and get you signup coupon</p>
-                <div class="d-flex justify-content-center">
+                <div className="d-flex justify-content-center">
                   <div className={styles.container_4}>
                     <form action="" method="post">
-                      <div class="input-group">
-                        <input type="email" class="form-control form-control-lg border-0" placeholder='Your email' aria-label="Your email" />
+                      <div className="input-group">
+                        <input type="email" className="form-control form-control-lg border-0" placeholder='Your email' aria-label="Your email" />
                         <a type="submit">
                           <div className={styles.button_3}>
-                            <div class="d-flex align-items-center justify-content-center" style={{ height: 57, width: 147 }}>
+                            <div className="d-flex align-items-center justify-content-center" style={{ height: 57, width: 147 }}>
                               Book now
                             </div>
                           </div>
@@ -373,7 +373,7 @@ export default function Home() {
         <div className={`container-fluid py-5`}>
           <div className='row mx-0'>
             <div className='col-lg-2 mb-4'>
-              <div class="d-flex justify-content-center">
+              <div className="d-flex justify-content-center">
                 <Image
                   src="/images/techy_2.png"
                   alt="Image"
@@ -383,7 +383,7 @@ export default function Home() {
               </div>
             </div>
             <div className='col-lg-8 mb-4'>
-              <div class="d-flex flex-row justify-content-center">
+              <div className="d-flex flex-row justify-content-center">
                 <div className='px-2'>
                   <a href="" className={styles.text_18}>Terms and Conditions</a>
                 </div>
@@ -393,26 +393,26 @@ export default function Home() {
               </div>
             </div>
             <div className='col-lg-2 mb-4'>
-              <div class="d-flex flex-row justify-content-center">
+              <div className="d-flex flex-row justify-content-center">
                 <div className='px-3'>
                   <a href='https://facebook.com/'>
-                    <i class="fa-brands fa-facebook fa-2x text-white"></i>
+                    <i className="fa-brands fa-facebook fa-2x text-white"></i>
                   </a>
                 </div>
                 <div className='px-3'>
                   <a href='https://linkedin.com/'>
-                    <i class="fa-brands fa-linkedin-in fa-2x text-white"></i>
+                    <i className="fa-brands fa-linkedin-in fa-2x text-white"></i>
                   </a>
                 </div>
                 <div className='px-3'>
                   <a href='https://twitter.com/'>
-                    <i class="fa-brands fa-twitter fa-2x text-white"></i>
+                    <i className="fa-brands fa-twitter fa-2x text-white"></i>
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div class="d-flex justify-content-center">
+          <div className="d-flex justify-content-center">
             <div>
               <span className={styles.text_19}>2022</span>
             </div>
@@ -420,7 +420,7 @@ export default function Home() {
         </div>
 
       </footer>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
     </div>
   )
 }
