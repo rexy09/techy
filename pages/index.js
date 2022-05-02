6b396165ef6744b381379c1adc910de29c91644f
rexy09 <fredmuju@gmail.com>
2022-05-02 17:08:30 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Techy</title>
-        <meta name="description" content="Techy" />
-        <link rel="icon" href="/images/techy_1.png" />
+        <title>Devhat</title>
+        <meta name="description" content="Devhat" />
+        <link rel="icon" href="/images/devhat.png" />
 
         {/* Boostrap */}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
@@ -19,12 +19,13 @@ export default function Home() {
       <nav className="navbar navbar-expand-lg navbar-light bg-white" id='#'>
         <div className="container-fluid px-lg-5 py-3">
           <a className="navbar-brand" href="#">
-            <Image
+            <h1>Devhat</h1>
+            {/* <Image
               src="/images/techy_1.png"
               alt="Image"
               width={86}
               height={43}
-            />
+            /> */}
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -53,16 +54,18 @@ export default function Home() {
         </div>
       </nav>
       <div className={styles.bg_img_5}>
-        <div className='container mb-5'>
+        <div className='container-fluid mb-5'>
           <div className={styles.container_1}>
             <div className='row py-4 px-lg-5'>
-              <div className='col-lg-6'>
+              <div className='col-lg-6 mb-4'>
                 <div className="d-flex justify-content-center">
                   <div className='' style={{ maxWidth: '100%'}}>
                     <h1 className={styles.text_1}>Let<span>&#39;</span>s make you,</h1>
                     <h1 className={styles.text_1}>an eye catching</h1>
                     <h1 className={styles.text_2}>designs.</h1>
-                    <p className={styles.text_3}>Stand out from the rest with your very own <br />
+                    <p className={`d-none d-sm-none d-md-block ${styles.text_3}`}>Stand out from the rest with your very own <br />
+                      elegant and unique design.</p>
+                    <p className={`d-block d-sm-block d-md-none ${styles.text_3}`} style={{maxWidth:300}}>Stand out from the rest with your very own
                       elegant and unique design.</p>
                     <a className="" href="#">
                       <div className={styles.button_1}>
@@ -167,32 +170,28 @@ export default function Home() {
         <section id='technologies'>
           <div className='container-fluid'>
             <div className='row py-5 mt-5 mx-0'>
-              <div className='col-lg-6'>
+              <div className='col-lg-6 mb-4'>
                 <div className="d-flex justify-content-center">
                   <Image
                     src="/images/Group_1.png"
                     alt="Image"
-                    width={554}
-                    height={571}
+                    width={505}
+                    height={541}
                   />
                 </div>
               </div>
-              <div className='col-lg-6'>
-                <div className={styles.bg_img_1}>
+              <div className='col-lg-6 mb-4'>
+                <div className={styles.bg_img_1} style={{maxWidth:600}}>
                   <h1 className={styles.text_6}>How we use these,</h1>
                   <h1 className={styles.text_7}>Technologies</h1>
                   <p className={styles.text_8}>In a professional context it often happens that private or
-                    <br />
                     corporate clients corder a publication to be made and
-                    <br />
                     presented with the actual content still not being ready.
-                    <br />
                     Think of a news blog that<span>&#39;</span>s filled with content hourly on
-                    <br />
                     the day of going live. </p>
                   <a className="" href="#">
                     <div className={styles.button_2}>
-                      <div className="d-flex align-items-center justify-content-center" style={{ height: 59, width: 302 }}>
+                      <div className="d-flex align-items-center justify-content-center" style={{ height: 59, width: 300 }}>
                         <div>
                           Book now <i className="fa-solid fa-arrow-right-long fa-lg ps-2"></i>
                         </div>
@@ -214,7 +213,9 @@ export default function Home() {
 
             </div>
           </div>
-          <p className={styles.text_11}>we work as a Python development agency for 13+ years and are able to cover  <br />the business may need</p>
+          <p className={`d-none d-md-none d-lg-block ${styles.text_11}`}>we work as a Python development agency for 13+ years and are able to cover  <br />the business may need</p>
+          <p className={`d-block d-md-block d-lg-none ${styles.text_11}`}>we work as a Python development agency for 13+ years and are able to cover the business may need</p>
+
           <div className={styles.bg_img_2}>
             <div className='row py-lg-5 mt-lg-5 mx-0'>
               <div className='col-lg-4 d-flex justify-content-center mb-4'>
@@ -289,20 +290,16 @@ export default function Home() {
 
       <section id='cloud'>
         <div className={`container ${styles.bg_img_3}`}>
-          <div className='row py-5 mt-5 mx-0'>
+          <div className='row py-5 my-5 mx-0'>
             <div className='col-lg-6 mb-4'>
               <div className="d-flex justify-content-center">
                 <div >
                   <h1 className={styles.text_6}>Our</h1>
                   <h1 className={`${styles.text_9} mb-5`}>Cloud Providers</h1>
                   <p className={styles.text_8}>In a professional context it often happens that private or
-                    <br />
                     corporate clients corder a publication to be made and
-                    <br />
                     presented with the actual content still not being ready.
-                    <br />
                     Think of a news blog that<span>&#39;</span>s filled with content hourly on
-                    <br />
                     the day of going live. </p>
                   <a className="" href="#">
                     <div className={styles.button_2}>
@@ -321,8 +318,8 @@ export default function Home() {
                 <Image
                   src="/images/Group_3.png"
                   alt="Image"
-                  width={661}
-                  height={415}
+                  width={578}
+                  height={413}
                 />
               </div>
             </div>
@@ -344,6 +341,7 @@ export default function Home() {
                 </div>
                 <p className={styles.text_17}>Enter your email address to book now
                   <br />and get you signup coupon</p>
+
                 <div className="d-flex justify-content-center">
                   <div className={styles.container_4}>
                     <form action="" method="post">
@@ -351,7 +349,7 @@ export default function Home() {
                         <input type="email" className="form-control form-control-lg border-0" placeholder='Your email' aria-label="Your email" />
                         <a type="submit">
                           <div className={styles.button_3}>
-                            <div className="d-flex align-items-center justify-content-center" style={{ height: 57, width: 147 }}>
+                            <div className="d-flex align-items-center justify-content-center" style={{ height: 57, maxWidth: 147 }}>
                               Book now
                             </div>
                           </div>
@@ -373,12 +371,13 @@ export default function Home() {
           <div className='row mx-0'>
             <div className='col-lg-2 mb-4'>
               <div className="d-flex justify-content-center">
-                <Image
+                <h1 className='text-light'>Devhat</h1>
+                {/* <Image
                   src="/images/techy_2.png"
                   alt="Image"
                   width={82}
                   height={41}
-                />
+                /> */}
               </div>
             </div>
             <div className='col-lg-8 mb-4'>
